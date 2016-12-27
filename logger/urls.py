fbcb536d7cdb18c5d28ed6cea2c171118250b6dc
fiehl <fiehl@philsmile.com>
2016-12-28 02:07:35 +0800

    added view and template for detailed logs

--- logger/urls.py
+++ logger/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
 	url(r'^$', views.logs_list, name='logs_list'),
+	url(r'^logs/(?P<pk>\d+)/$',views.logs_detail, name="logs_detail"),
 ]
--- logger/views.py
+++ logger/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
 from .models import Project,Logs 
 from django.utils import timezone
-
+from django.shortcuts import render, get_object_or_404
 # Create your views here.
 def logs_list(request):
 	logs = Logs.objects.filter(date__gte=timezone.now()).order_by('-date')
 	return render(request,'logs/logs_list.html',{'logs':logs})
+
+def logs_detail(request,pk):
+	log = get_object_or_404(Logs, pk=pk)
+	print log
+	return render(request, 'logs/log_detail.html', {'log':log})
